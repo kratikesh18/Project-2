c869acab0eec4633716105df8f0abb981f68b594
kratikesh18 <im.kartikesh18@gmail.com>
2024-05-02 17:43:27 +0530

    added maintainence page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-
+import second from "./errorImage.png";
 const App = () => {
   return (
     <div className="h-screen w-full bg-gradient-to-l flex flex-col justify-between items-center from-purple-600 to to-cyan-500 ">
       <div className="flex flex-col gap-4 justify-center items-center pt-8  md:flex-row">
-        <img
-          src="https://github.com/kratikesh18/Project-2/blob/main/src/error-removebg-preview.png"
-          alt="Error section"
-        />
+        <img src={second} alt="Error section" />
         <div className="flex flex-col gap-4 text-white text-center ">
           <h1 className=" font-bold tracking-wide  text-3xl">
             Hey! We are building something special.
